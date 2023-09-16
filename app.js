e73090468e607947884ad2e93fd3091230cf454f
minidukk <vanducg15@gmail.com>
2023-09-16 10:42:22 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -11,5 +12,18 @@ app.use("/app/contacts", contactsRouter);
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application. lmao" });
 });
+
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+//defind error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
+
 
 module.exports = app;
