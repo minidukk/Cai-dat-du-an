2cf035d6cd9057d58e8851d3a27687eaa7c8f31b
minidukk <vanducg15@gmail.com>
2023-10-04 11:17:28 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/app/contacts", contactsRouter);
+
 
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application. lmao" });
+    res.json({ message: "Welcome to contact book application." });
 });
-
+app.use("/api/contacts", contactsRouter);
 // handle 404 response
 app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
 
-//defind error-handling middleware last, after other app.use() and routes calls
+//define error-handling middleware last, after other app.use() and routes calls
 app.use((err, req, res, next) => {
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
     });
 });
 
